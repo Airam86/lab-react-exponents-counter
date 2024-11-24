ae1b6def70f9de4b39c62b021cd83b3db596d2d9
Airam Santos diaz <airam@macbook-pro-de-airam-1.home>
2024-11-24 20:17:09 +0100

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,44 @@ import ExponentThree from "./components/ExponentThree";
 import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
+import { useState } from "react";
 
 
-function App () {
+function App() {
+  const [count, setCount] = useState(0);
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);
+
   return (
     <div className="App">
-      <h2><em>Counter</em></h2>
-  
-      <Counter/>
+      <h2>
+        <em>Counter</em>
+      </h2>
+
+      <Counter count={count} decrement={decrement} increment={increment} />
 
       <br />
-      <h2><em>Exponents</em></h2>
+      <h2>
+        <em>Exponents</em>
+      </h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+     <ExponentTwo count={count} />
+        <ExponentThree count={count} />
+        <ExponentFour count={count} />
+        <ExponentFive count={count} />
+        <ExponentSix count={count} />    
+
+        {/* Al hacer el bonus, me daba un error por importar exponentes que no usaba, por eso lo hice, y lo comenté para evitar errores.*/}
+         {/*BONUS
+        <Exponent count={count} exponent = {2} />
+        <Exponent count={count} exponent = {3} />
+        <Exponent count={count} exponent = {4} />
+        <Exponent count={count} exponent = {5} />       
+        <Exponent count={count} exponent = {6} />
+        <Exponent count={count} exponent = {7} /> */}
+
+
       </div>
     </div>
   );
